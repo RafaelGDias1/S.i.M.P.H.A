72e80ea9dfc4a83520d903ccb77c6ef9b53b7594
Rafael Gonçalves Dias <119137768+RafaelGDias1@users.noreply.github.com>
2023-10-30 17:47:34 -0300

    atualização da primeira pag

--- pag1.php
+++ pag1.php
@@ -44,6 +44,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="estilo.css">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </head>
@@ -60,19 +61,86 @@
 
         <ul>
             <li><a href="telalogin.php">Entrar</a></li>
-            <li><a href="cadastrar.php">Criar uma conta</a></li>
+            <li><a href="cadastrar.php">Cria uma conta</a></li>
            
         </ul>
        
     </nav>
 
+<main>
 
 
 
+</main>
+
+    <footer>
+        <div id="fundo_content">
+            <div id="fundo_contacts">
+                <h1>Logo</h1>
+                <p>alalalalalalala</p>
+
+                <div id="fundo_social_media">
+                  <a href="#" class="fundo-link" id="instagram">
+                    <i class="fa-brands fa-instagram"></i>
+                  </a>
+
+                  <a href="#" class="fundo-link" id="facebook">
+                    <i class="fa-brands fa-facebook"></i>
+                  </a>
+
+                  <a href="#" class="fundo-link" id="whatsapp">
+                    <i class="fa-brands fa-whatsapp"></i>
+                  </a>
+                </div>
+            </div>
+            <ul class="fundo-list">
+                <li>
+                    <h3>Blog</h3>
+                </li>
+                <li>
+                    <a href="#" class="fundo-link">Tech</a>
+                </li>
+                <li>
+                    <a href="#" class="fundo-link">Adventures</a>
+                </li>
+                <li>
+                    <a href="#" class="fundo-link">Music</a>
+                </li>
+            </ul>
+            <ul class="fundo-list">
+                <li>
+                    <h3>Products</h3>
+                </li>
+                <li>
+                    <a href="#" class="fundo-link">App</a>
+                </li>
+                <li>
+                    <a href="#" class="fundo-link">Desktop</a>
+                </li>
+                <li>
+                    <a href="#" class="fundo-link">Cloud</a>
+                </li>
+            </ul>
+
+            <div id="fundo_subscribe">
+                <h3>Subscribe</h3>
+                <p>lalalalalallalalalaalla</p>
+                <div id="input_group">
+                  <input type="email" id="email">
+                  <button>
+                  <i class="fa-regular fa-envelope"></i>
+
+                  </button>
+                </div>
+            </div>
+        </div>
+     <div id="fundo_copyrigth">
+        &#169
+        2023 all rights reserved
+     </div>
+    </footer>
 
 
-
-
-        </form>
-    </body>
- </html>
+</form>
+</body>
+</html>
